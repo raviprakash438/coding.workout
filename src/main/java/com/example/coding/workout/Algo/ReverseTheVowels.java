f52358238f6354b300f8082d01209f4d6961430c
raviprakash438 <Happy@2209>
2021-05-21 20:58:31 +0530

    ReverseTheVowels

---
+++ src/main/java/com/example/coding/workout/Algo/ReverseTheVowels.java
@@ -0,0 +1,49 @@
+package com.example.coding.workout.Algo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReverseTheVowels {
+
+    static String str = "search";
+
+    public static void main(String[] args) {
+        System.out.println("input: " + str);
+        System.out.println("output: " + reverse(str));
+    }
+
+    public static String reverse(String str) {
+
+        char[] chararr = str.toCharArray();
+        for (int i = 0, j = chararr.length - 1; i < chararr.length; i++) {
+            if (isVowel(chararr[i])) {
+
+                while (j > i && !isVowel(chararr[j])) {
+                    j--;
+                }
+                if (j > i && isVowel(chararr[j])) {
+                    char temp = chararr[i];
+                    chararr[i] = chararr[j];
+                    chararr[j] = temp;
+                    j--;
+                }
+            }
+
+        }
+
+        return String.valueOf(chararr);
+    }
+
+    private static boolean isVowel(char c) {
+
+        List<Character> vowels = new ArrayList<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+
+        return vowels.contains(c);
+    }
+}
